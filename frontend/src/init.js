91957db7217c9bd47f4c31373139793aabf75762
constvict <constantinumnov@gmail.com>
2023-05-22 17:50:19 +0700

    add 18next

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import i18next from 'i18next';
+import { initReactI18next, I18nextProvider } from 'react-i18next';
 import SocketProvider from './contexts/SocketProvider.jsx';
 import App from './components/App.jsx';
 import store from './slices/index.js';
 import initSocket from './socket.js';
+import resources from './locales/index.js';
 
 const init = async () => {
   const socket = initSocket();
+  const i18n = i18next.createInstance();
+
+  await i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      fallbackLng: 'ru',
+    });
 
   return (
     <Provider store={store}>
       <SocketProvider socket={socket}>
-        <App />
+        <I18nextProvider i18n={i18n}>
+          <App />
+        </I18nextProvider>
       </SocketProvider>
     </Provider>
   );
