125ceea8ea50b2f197e4d835fb2d936c34294b25
constvict <constantinumnov@gmail.com>
2023-05-23 16:23:00 +0700

    add rollbar

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import i18next from 'i18next';
 import leoProfanity from 'leo-profanity';
 import { initReactI18next, I18nextProvider } from 'react-i18next';
@@ -8,6 +9,15 @@ import App from './components/App.jsx';
 import store from './slices/index.js';
 import initSocket from './socket.js';
 import resources from './locales/index.js';
+
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
+};
 
 const init = async () => {
   const socket = initSocket();
@@ -25,13 +35,17 @@ const init = async () => {
   leoProfanity.add(leoProfanity.getDictionary('ru'));
 
   return (
-    <Provider store={store}>
-      <SocketProvider socket={socket}>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </SocketProvider>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <SocketProvider socket={socket}>
+            <I18nextProvider i18n={i18n}>
+              <App />
+            </I18nextProvider>
+          </SocketProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
